f5fa15ab2feb29eece46b8fc340e67b64cf3c8d8
niv <braniv@gmail.com>
2017-06-17 11:37:48 +0300

    05 titleize

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
-# write your code here
+	attr_accessor :title
+
+	def titleize s
+		# assuming one space between each word exactly
+		little_words = ['and','to','is','be', 'over', 'the', 'of', 'in','a','an']
+		new_s = ''
+		s.split.each do |word|
+			first_letter = if little_words.include? word then word[0] else word[0].upcase end
+			new_s += first_letter+word[1..-1] + ' '
+		end
+		return new_s[0].upcase+new_s[1...-1]
+		
+	end
+
+	def title=(new_title)
+	    @title = titleize new_title
+  	end
 end
