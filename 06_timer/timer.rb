5444f0bdd7cc6ae22242c339306ee5577a23ec0b
niv <braniv@gmail.com>
2017-06-17 11:53:20 +0300

    and done

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,23 @@
 class Timer
-  #write your code here
+	attr_accessor :seconds
+
+	def initialize
+		@seconds = 0
+	end
+
+	def padzero num
+		if num<10 then
+			return "0#{num}"
+		else
+			return "#{num}"
+		end
+	end
+
+	def time_string
+		ts = ""
+		hours = @seconds / (60*60)
+		minutes = ( @seconds / 60 ) % 60
+		seconds = ( @seconds ) % 60
+		return "#{padzero hours}:#{padzero minutes}:#{padzero seconds}"
+	end
 end
